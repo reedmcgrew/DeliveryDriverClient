59ed3b01af2cea028b043b6c0d5ce687392b7e9e
Reed McGrew <reedmcgrew1185@gmail.com>
2012-07-28 21:43:00 -0600

    Removed settings file from version control.

--- settings.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created with JetBrains WebStorm.
- * User: reed
- * Date: 7/28/12
- * Time: 9:41 PM
- * To change this template use File | Settings | File Templates.
- */
-module.exports = {};
